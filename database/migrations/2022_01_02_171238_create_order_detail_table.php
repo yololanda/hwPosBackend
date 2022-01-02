27fbf55aef84bddd33e27153717f5a422bf281ce
dev <toin.chang31@gmail.com>
2022-01-03 00:16:47 +0700

    add order migration

---
+++ database/migrations/2022_01_02_171238_create_order_detail_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_detail', function (Blueprint $table) {
+            $table->id();
+            $table->integer('order_id');
+            $table->integer('product_id');
+            $table->string('model');
+            $table->integer('quantity');
+            $table->bigInteger('price');
+            $table->bigInteger('subtotal');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_detail');
+    }
+}
